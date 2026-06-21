8d326fc907bbb09cb5f8ba55f0333e48db48dff2
Alaa Almadadha <alaaalmadadha@gmail.com>
2026-06-22 02:03:24 +0300

    Stabilize CI locale checks

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -30,6 +30,7 @@ class ProfileTest extends TestCase
             ->patch('/profile', [
                 'name' => 'Test User',
                 'email' => 'test@example.com',
+                'lang' => 'en',
             ]);
 
         $response
@@ -52,6 +53,7 @@ class ProfileTest extends TestCase
             ->patch('/profile', [
                 'name' => 'Test User',
                 'email' => $user->email,
+                'lang' => 'en',
             ]);
 
         $response
