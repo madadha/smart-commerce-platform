0559cfaef402590893d41fbee08422df4509feda
Alaa Almadadha <alaaalmadadha@gmail.com>
2026-06-23 13:33:22 +0300

    Fix profile locale navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,24 @@
 @php
     $locale = request('lang', session('storefront_locale', 'ar'));
     $locale = in_array($locale, ['ar', 'he', 'en'], true) ? $locale : 'ar';
+
+    $labels = [
+        'ar' => [
+            'account' => 'حسابي',
+            'profile' => 'بياناتي',
+            'logout' => 'تسجيل الخروج',
+        ],
+        'he' => [
+            'account' => 'החשבון שלי',
+            'profile' => 'הפרופיל שלי',
+            'logout' => 'התנתקות',
+        ],
+        'en' => [
+            'account' => 'My Account',
+            'profile' => 'Profile',
+            'logout' => 'Logout',
+        ],
+    ][$locale];
 @endphp
 
 <nav class="scp-mini-account-nav">
@@ -15,16 +33,16 @@
 
         <div>
             <a href="{{ route('storefront.account.dashboard', ['lang' => $locale]) }}">
-                {{ __('storefront.account_dashboard.my_account') }}
+                {{ $labels['account'] }}
             </a>
             <a href="{{ route('profile.edit', ['lang' => $locale]) }}">
-                {{ $locale === 'ar' ? 'بياناتي' : ($locale === 'he' ? 'הפרופיל שלי' : 'Profile') }}
+                {{ $labels['profile'] }}
             </a>
 
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <button type="submit">
-                    {{ $locale === 'ar' ? 'تسجيل خروج' : ($locale === 'he' ? 'יציאה' : 'Logout') }}
+                    {{ $labels['logout'] }}
                 </button>
             </form>
         </div>
