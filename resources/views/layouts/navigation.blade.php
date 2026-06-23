b9ffa6e2108067b996f7522216bdc66ada960e38
Alaa Almadadha <alaaalmadadha@gmail.com>
2026-06-23 13:41:15 +0300

    Move account links below header label

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,16 +4,19 @@
 
     $labels = [
         'ar' => [
+            'section' => 'الحساب',
             'account' => 'حسابي',
             'profile' => 'بياناتي',
             'logout' => 'تسجيل الخروج',
         ],
         'he' => [
+            'section' => 'החשבון',
             'account' => 'החשבון שלי',
             'profile' => 'הפרופיל שלי',
             'logout' => 'התנתקות',
         ],
         'en' => [
+            'section' => 'Account',
             'account' => 'My Account',
             'profile' => 'Profile',
             'logout' => 'Logout',
@@ -23,28 +26,24 @@
 
 <nav class="scp-mini-account-nav">
     <div class="scp-container">
-        <a href="{{ route('storefront.home', ['lang' => $locale]) }}" class="scp-logo">
-            <span class="scp-logo-mark">S</span>
-            <span>
-                <strong>Smart Commerce</strong>
-                <small>Marketplace Platform</small>
-            </span>
-        </a>
+        <div class="scp-mini-account-panel">
+            <span class="scp-mini-account-title">{{ $labels['section'] }}</span>
 
-        <div>
-            <a href="{{ route('storefront.account.dashboard', ['lang' => $locale]) }}">
-                {{ $labels['account'] }}
-            </a>
-            <a href="{{ route('profile.edit', ['lang' => $locale]) }}">
-                {{ $labels['profile'] }}
-            </a>
+            <div class="scp-mini-account-links">
+                <a href="{{ route('storefront.account.dashboard', ['lang' => $locale]) }}">
+                    {{ $labels['account'] }}
+                </a>
+                <a href="{{ route('profile.edit', ['lang' => $locale]) }}">
+                    {{ $labels['profile'] }}
+                </a>
 
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit">
-                    {{ $labels['logout'] }}
-                </button>
-            </form>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit">
+                        {{ $labels['logout'] }}
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 </nav>
